b387bb339e0898be6d62b5080748c8e1e3de96fd
Tony Tan <doublskyp@gmail.com>
2017-04-23 22:23:22 -0500

    fix missing MFA in cla_nci

--- rtl/vbw_cla.v
+++ rtl/vbw_cla.v
@@ -87,11 +87,10 @@ module cla_adder
             wire    [7:0]       p1, g1, ci1;
             wire    [1:0]       p2, g2, ci2;
             wire                p3, g3;
-            wire    [1:0]       dummy;
             
             LA4 layer1 [7:0]    (g, p, ci1, g1, p1, internal_ci);
             LA4 layer2 [1:0]    (g1, p1, ci2, g2, p2, ci1);
-            LA4 layer3          ({2'b0,g2}, {2'b0,p2}, ci, g3, p3, {dummy,ci2});
+            LA2 layer3          (g2, p2, ci, g3, p3, ci2);
 
             assign co = g3 | (p3 & ci);
         end
@@ -106,10 +105,9 @@ module cla_adder
         else if (WIDTH == 8) begin
             wire    [1:0]       p1, g1, ci1;
             wire                p2, g2;
-            wire    [1:0]       dummy;
             
             LA4 layer1 [1:0]    (g, p, ci1, g1, p1, internal_ci);
-            LA4 layer2          ({2'b0,g1}, {2'b0,p1}, ci, g2, p2, {dummy,ci1});
+            LA2 layer2          (g1, p1, ci, g2, p2, ci1);
             assign co = g2 | (p2 & ci);
         end
     endgenerate
@@ -217,7 +215,7 @@ module vbw_cla_nci
     input       [63:0]  b,
     input               ci,
     input       [1:0]   control,
-    output  reg [63:0]  s,
+    output      [63:0]  s,
     output              co
 );
 
@@ -229,6 +227,8 @@ module vbw_cla_nci
     wire    [1:0]   p6, g6, ci6, ci6_controlled;
     wire            p7, g7;
     wire            ci_controlled, co_controlled;
+
+    MFA mfa_inst [63:0] (a, b, ci1, p1, g1, s);
 
     LA2 layer1 [31:0] (g1, p1, ci2, g2, p2, ci1);
     LA2 layer2 [15:0] (g2, p2, ci3, g3, p3, ci2);
